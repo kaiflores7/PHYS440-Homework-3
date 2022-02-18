7c633ce60b621b904cce6e6db478c1680a3d2a55
kaiflores7 <kflores7@hawk.iit.edu>
2022-02-18 11:42:59 -0600

    Initial Commit

---
+++ Shared/Homework_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_3App.swift
+//  Shared
+//
+//  Created by Katia Flores on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
